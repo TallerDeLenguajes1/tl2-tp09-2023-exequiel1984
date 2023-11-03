488860eab8324b334b760b36bb699241c09a493a
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2023-11-03 19:04:27 -0300

    creo interface usuario

---
+++ Repository/IUsuarioRepository.cs
@@ -0,0 +1,11 @@
+namespace Practico9
+{
+    public interface IUsuarioRepository
+    {
+        public void Create(Usuario usuario);
+        public void Update(int id, Usuario usuario);
+        public List<Usuario> GetAll();
+        public void GetById(int id);
+        public void Remove(int id);
+    }
+}
